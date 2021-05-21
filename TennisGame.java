2b6ffb54223aec0250b8e03d8fa54877cb9ed98d
trav-d13 <t.dawson@student.maastrichtuniversity.nl>
2021-05-21 10:43:43 +0200

    Method breakdown

--- TennisGame.java
+++ TennisGame.java
@@ -8,47 +8,19 @@ public class TennisGame {
     private String player1Name;
     private String player2Name;
 
-    public TennisGame(String player1Name, String player2Name) {
+    public TennisGame(String player1Name, String player2Name)
+    {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
     public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = "Love";
-            if (P1point == 1)
-                score = "Fifteen";
-            if (P1point == 2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point == P2point && P1point >= 3)
-            score = "Deuce";
 
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = "Fifteen";
-            if (P1point == 2)
-                P1res = "Thirty";
-            if (P1point == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = "Fifteen";
-            if (P2point == 2)
-                P2res = "Thirty";
-            if (P2point == 3)
-                P2res = "Forty";
-
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
+        score = determineTiedScores();
+        score = determineDeuce();
+        score = p1Love();
+        score = p2Love();
 
         if (P1point > P2point && P1point < 4) {
             if (P1point == 2)
@@ -87,6 +59,7 @@ public class TennisGame {
         if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
             score = "Win for player2";
         }
+
         return score;
     }
 
